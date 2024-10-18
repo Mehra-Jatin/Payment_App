d86553bfb93c2291034098b9645788cb80ba6aba
Mehra-Jatin <jatinmehra578@gmail.com>
2024-10-18 14:38:01 +0530

    SignIn

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -10,9 +10,7 @@ function SignIn() {
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
         <Heading label={"Sign In"} />
-        <SubHeading label={"Enter your infromation to create an account"} />
-        <InputBox placeholder="John" label={"First Name"} />
-        <InputBox placeholder="Doe" label={"Last Name"} />
+        <SubHeading label={"Enter your credentials to access your account"}  />
         <InputBox placeholder="user@gmail.com" label={"Email"} />
         <InputBox placeholder="123456" label={"Password"} />
         <div className="pt-4">
