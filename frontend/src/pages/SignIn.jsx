d0b66bb44bfd607284fc63e46695f3738ca2ebc3
Mehra-Jatin <jatinmehra578@gmail.com>
2024-10-18 14:26:30 +0530

    SignIn

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import Heading from '../components/Heading'
+import SubHeading from '../components/Subheading'
+import InputBox from '../components/InputBox'
+import Button from '../components/Button'
+import Warning from '../components/Warning'
 function SignIn() {
   return (
-    <div className="bg-slate-300 h-screen flex justify-center">
-     <Heading label={"Sign In"}/>
+   <div className="bg-slate-300 h-screen flex justify-center">
+    <div className="flex flex-col justify-center">
+      <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+        <Heading label={"Sign In"} />
+        <SubHeading label={"Enter your infromation to create an account"} />
+        <InputBox placeholder="John" label={"First Name"} />
+        <InputBox placeholder="Doe" label={"Last Name"} />
+        <InputBox placeholder="user@gmail.com" label={"Email"} />
+        <InputBox placeholder="123456" label={"Password"} />
+        <div className="pt-4">
+          <Button label={"Sign In"} />
+        </div>
+        <Warning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
+      </div>
     </div>
+  </div>
+
   )
 }
 
