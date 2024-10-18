3110a9bb19fd0b1ef8fa80da7587e2176ef39155
Mehra-Jatin <jatinmehra578@gmail.com>
2024-10-18 13:53:56 +0530

    routes signup signin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 const App = () => {
   return (
-    <div>
-      hello
-    </div>
+     <Router>
+        <Routes>
+          <Route path="/" element={<h1>Hello World</h1>} />
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/signin" element={<SignIn/>} />
+        </Routes>
+      </Router>
   )
 }
 
