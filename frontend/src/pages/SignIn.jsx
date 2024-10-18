f3ca91c0639a38948ad8e10aaf32f12458226559
Mehra-Jatin <jatinmehra578@gmail.com>
2024-10-18 14:04:40 +0530

    Heading

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Heading from '../components/Heading'
 function SignIn() {
   return (
-    <div>
-       SignIn
+    <div className="bg-slate-300 h-screen flex justify-center">
+     <Heading label={"Sign In"}/>
     </div>
   )
 }
