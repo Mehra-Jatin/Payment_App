e53aebdc6068a952ac359d7171315c16f58b2bb4
Mehra-Jatin <jatinmehra578@gmail.com>
2024-10-22 15:52:08 +0530

    modified: appbar

--- frontend/src/components/AppBar.jsx
+++ frontend/src/components/AppBar.jsx
@@ -4,7 +4,7 @@ function AppBar() {
   return (
     <div className="shadow h-14 flex justify-between">
     <div className="flex flex-col justify-center h-full ml-4">
-        PayTM App
+        Payment App
     </div>
     <div className="flex">
         <div className="flex flex-col justify-center h-full mr-4">
