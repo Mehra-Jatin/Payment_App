ad2a1cd22b32f1513f36c901ed21c3540def309b
Mehra-Jatin <jatinmehra578@gmail.com>
2024-10-23 21:05:09 +0530

    created: DashBoard

---
+++ frontend/src/pages/DashBoard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AppBar from '../components/AppBar'
+import Balance from '../components/Balance'
+import  Users  from '../components/Users'
+function DashBoard() {
+  return (
+    <div>
+       <AppBar />
+        <div className="m-8">
+            <Balance value={"10,000"} />
+            <Users />
+       </div>
+    </div>
+  )
+}
+
+export default DashBoard
