04a81df69a96d01d479819052e3769bd8612718f
Mehra-Jatin <jatinmehra578@gmail.com>
2024-10-23 21:03:40 +0530

    modified: action

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Button({label, onClick}) {
+function Button({label, action}) {
   return (
-       <button onClick={onClick} type="button" class=" w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">{label}</button>
+       <button onClick={action} type="button" class=" w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">{label}</button>
       )
 }
 
