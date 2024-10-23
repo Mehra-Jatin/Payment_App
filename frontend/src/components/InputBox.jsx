04fcda8effe582984ad1c448b846f9ed6c7a5120
Mehra-Jatin <jatinmehra578@gmail.com>
2024-10-23 22:03:13 +0530

    Modified: onchange

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function InputBox({label, placeholder}) {
+function InputBox({label, placeholder,action}) {
   return (
     <div>
       <div className="text-sm font-medium text-left py-2">
         {label}
       </div>
-      <input placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
+      <input onChange={action} placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
     </div>
   )
 }
