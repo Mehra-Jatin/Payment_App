f9f7907ec6e9b449afdbb11bebdcc20f24dd4c65
Mehra-Jatin <jatinmehra578@gmail.com>
2024-10-23 21:26:53 +0530

    Created:History

---
+++ frontend/src/pages/History.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppBar from '../components/AppBar'
+function History() {
+  return (
+    <div>
+      <AppBar />
+      <h1>History</h1>
+    </div>
+  )
+}
+
+export default History
